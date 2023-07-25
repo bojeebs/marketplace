d5f704150104a6d818a35d0c1afea9facc2f795d
Tyler Carter <113203952+bojeebs@users.noreply.github.com>
2023-07-25 14:21:04 -0500

    added auth queries now testing

--- Backend/middleware/index.js
+++ Backend/middleware/index.js
@@ -4,3 +4,71 @@ const SALT_ROUNDS = 12
 const APP_SECRET = 'supersecretkey'
 
 
+const hashPassword = async (password) => {
+let hashedPassword = await bcrypt.hash(password, SALT_ROUNDS)
+
+return hashedPassword
+}
+
+const comparePassword = async (storedPassword, password) => {
+  let passwordMatch = await bcrypt.compare(password, storedPassword)
+  return passwordMatch 
+}
+
+const createToken = (payload) => {
+  let token = jwt.sign(payload, APP_SECRET)
+  return token
+}
+
+const verifyToken = (req, res, next) => {
+  const { token } = res.locals
+  
+  let payload = jwt.verify(token, APP_SECRET)
+
+  if(payload){
+    res.locals.payload = payload
+
+    return next()
+  }
+  res.status(401).send({ status: 'Error', msg: 'Unauthorized'})
+}
+
+const stripToken = (req, res, next) => {
+  try {
+    const token = req.headers['authorization'].split(' ')[1]
+
+    if (token) {
+      res.locals.token = token
+    
+      return next()
+    }
+  } catch (error) {
+    res.status(401).send({ status: 'Error', msg: 'Unauthorized'})
+  }
+}
+
+const requireAuth = (req, res, next) => {
+  const { token } = res.locals
+  if (!token) {
+    return res.status(401).send({ status: 'Error', msg: "Unathorized"})
+  }
+
+  try {
+    const payload = jwt.verify(token, APP_SECRET)
+    res.locals.payload = payload
+    req.userId = payload.userId
+    next()
+  }catch (error) {
+    res.status(401).send ({ status: 'Error', msg: 'Unauthorized'})
+  }
+}
+
+
+module.exports = { 
+  stripToken,
+  verifyToken,
+  createToken,
+  comparePassword,
+  hashPassword,
+  requireAuth
+}
